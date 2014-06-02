5acb2a982225a7e79013c1c017f1394708a15330
gid <golikov@softclub.by>
2014-06-02 21:49:26 +0300

    создана страница p1

---
+++ src/main/java/ButtonView.java
@@ -0,0 +1,20 @@
+/**
+ * Created by gid_000 on 02.06.14.
+ */
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean
+public class ButtonView {
+
+    public void buttonAction(ActionEvent actionEvent) {
+        addMessage("ПРИВЕТ Илья!!");
+    }
+
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
